0bc92bc5bf15c475acc4afc72ffb684cc5a5f86a
YiChangChen <34483067+YiChangChen@users.noreply.github.com>
2019-07-03 15:27:48 +0800

    Add files via upload

---
+++ ICacheProvider.cs
@@ -0,0 +1,11 @@
+namespace Cache
+{
+    public interface ICacheProvider
+    {
+        object Get(string key);
+        void Set(string key, object data, int cacheTime);
+        bool IsSet(string key);
+        void Invalidate(string key);
+    }
+}
+
